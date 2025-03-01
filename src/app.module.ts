e49b4ad65691d65640279012fdaf330085ab34d7
Ghfshtfh291294@git <viethungtrieuphong1994@gmail.com>
2025-03-01 09:32:03 +0700

    connect to databasepandadb on aws

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,11 @@ import { MessageModule } from './messages/Message.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mssql',
-      host: '34.67.230.109',
+      host: process.env.DB_HOST,
       port: 1433,
-      username: 'sqlserver', //process.env.USERNAME_MSSQL
-      password: 'evenhubfully123456', //process.env.PASSWORD_MSSQL
-      database: 'evenhubDB', //process.env.NAMEDB_MSSQL
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       logging: true,
       // synchronize: true, ///not use production env
